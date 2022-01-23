c6bff2f1fb9b1f33decf5f33334f4d309c512d92
Minna <minna1025@gmail.com>
2022-01-23 20:42:24 +0900

    'as' and attrs

--- src/App.js
+++ src/App.js
@@ -4,21 +4,23 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
 `;
 
-const Circle = styled(Box)`
-  border-radius: 50px;
+const Input = styled.input.attrs({ required: true, minLength: 10 })`
+  background-color: tomato;
 `;
 
 function App() {
   return (
     <Father>
-      <Box bgColor="teal" />
-      <Circle bgColor="tomato" />
+      <Btn>Log in</Btn>
+      <Btn as="a">Log in</Btn>
+      <Input />
     </Father>
   );
 }
