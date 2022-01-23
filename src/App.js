e69e4ea9768c6766a0a3999314ab1c184b5fe6a7
Minna <minna1025@gmail.com>
2022-01-23 22:01:21 +0900

    Themes

--- src/App.js
+++ src/App.js
@@ -2,55 +2,21 @@ import styled, { keyframes } from "styled-components";
 
 const Wrapper = styled.div`
   display: flex;
+  width: 100vw;
+  height: 100vh;
   justify-content: center;
   align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
-const rotationAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0;
-  }
-  50% {
-    transform: rotate(360deg);
-    border-radius: 100px;
-  }
-  100% {
-    transform: rotate(0deg);
-    border-radius: 0;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  width: 100px;
-  height: 100px;
-  background-color: tomato;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation: ${rotationAnimation} 1s linear infinite;
-
-  ${Emoji} {
-    &:hover {
-      font-size: 98px;
-    }
-    &:active {
-      opacity: 0;
-    }
-  }
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji as="p">🤢</Emoji>
-      </Box>
-      <Emoji>💀</Emoji>
+      <Title>hello</Title>
     </Wrapper>
   );
 }
