70b089c65b9c003816f22f9c15df5180d2777d63
Minna <minna1025@gmail.com>
2022-01-24 14:25:48 +0900

    코드정리

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,32 +1,17 @@
 import styled from "styled-components";
 
-interface ContainerProps {
+interface CircleProps {
   bgColor: string;
 }
 
-const Container = styled.div<ContainerProps>`
+const Container = styled.div<CircleProps>`
   width: 200px;
   height: 200px;
   background-color: ${(props) => props.bgColor};
 `;
-
-interface CircleProps {
-  bgColor: string;
-}
 
 function Circle({ bgColor }: CircleProps) {
   return <Container bgColor={bgColor} />;
 }
 
 export default Circle;
-
-// interface PlayrShape {
-//   name: string;
-//   age: string;
-// }
-
-// const sayHello = (playerObj) =>
-//   `Hello ${playerObj.name} you are ${playerObj.age} years old.`;
-
-// sayHello({ name: "minna", age: 12 });
-// sayHello({ name: "nico", age: 12 });
