a7570efd7c44ba8835b51e7ea2ee1814cf15cd8c
Minna <minna1025@gmail.com>
2022-01-25 23:20:00 +0900

    CRYPTO TRACKER - price, chart part two

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,15 +1,56 @@
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
+
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
 
 interface ChartProps {
   coinId: string;
 }
 
 function Chart({ coinId }: ChartProps) {
-  const { isLoading, data } = useQuery(["ohlcv", coinId], () =>
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
     fetchCoinHistory(coinId)
   );
-  return <h1>Chart</h1>;
+
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart...."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[{ name: "price", data: data?.map((price) => price.close) }]}
+          options={{
+            chart: {
+              width: 500,
+              height: 500,
+              toolbar: { show: false },
+              background: "transparent",
+            },
+            grid: { show: false },
+            theme: { mode: "dark" },
+            stroke: { curve: "smooth", width: 4 },
+            xaxis: {
+              labels: { show: false },
+              axisTicks: { show: false },
+              axisBorder: { show: false },
+            },
+            yaxis: { show: false },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Chart;
