afa372fa01a923d31048f84edde6b2e653a0f489
Minna <minna1025@gmail.com>
2022-01-27 22:18:42 +0900

    STATE MANAGEMENT - custom validation(super crrrazy!)

--- todo/src/ToDoList.tsx
+++ todo/src/ToDoList.tsx
@@ -44,6 +44,7 @@ interface IForm {
   username: string;
   password: string;
   password1: string;
+  extraEror?: string;
 }
 
 function ToDoList() {
@@ -51,13 +52,21 @@ function ToDoList() {
     register,
     handleSubmit,
     formState: { errors },
+    setError,
   } = useForm<IForm>({
     defaultValues: {
       email: "@naver.com",
     },
   });
-  const onValid = (data: any) => {
-    console.log(data);
+  const onValid = (data: IForm) => {
+    if (data.password !== data.password1) {
+      setError(
+        "password1",
+        { message: "Password are not the same" },
+        { shouldFocus: true }
+      );
+    }
+    // setError("extraEror", { message: "Server offline" });
   };
 
   return (
@@ -77,7 +86,15 @@ function ToDoList() {
         />
         <span>{errors?.email?.message}</span>
         <input
-          {...register("firstName", { required: "First Name is required" })}
+          {...register("firstName", {
+            required: "First Name is required",
+            validate: {
+              noNico: (value) =>
+                value.includes("nico") ? "no nicos allowed" : true,
+              noNick: (value) =>
+                value.includes("nick") ? "no nick allowed" : true,
+            },
+          })}
           placeholder="First Name"
         />
         <span>{errors?.firstName?.message}</span>
@@ -111,6 +128,7 @@ function ToDoList() {
         />
         <span>{errors?.password1?.message}</span>
         <button>Add</button>
+        <span>{errors?.extraEror?.message}</span>
       </form>
     </div>
   );
