660ca2ae82723b638a80f6b12dc52832cb72627d
Minna <minna1025@gmail.com>
2022-01-28 21:55:21 +0900

    TRELLO CLONE - get selectors

--- trello/src/App.tsx
+++ trello/src/App.tsx
@@ -1,5 +1,26 @@
+import React from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { hourSelector, minuteState } from "./atoms";
+
 function App() {
-  return <div></div>;
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const hours = useRecoilValue(hourSelector);
+  const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+event.currentTarget.value); // +를 붙여 스트링을 넘버타입으로 변환
+  };
+
+  return (
+    <div>
+      <input
+        value={minutes}
+        onChange={onMinutesChange}
+        type="number"
+        placeholder="Minutes"
+      />
+      <input value={hours} type="number" placeholder="Hours" />
+      {/* onChange 이벤트가 없다면 리드온리 인풋임! */}
+    </div>
+  );
 }
 
 export default App;
