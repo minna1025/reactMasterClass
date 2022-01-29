c364777fbda64f1c48dc50ca2c8ff425fa080242
Minna <minna1025@gmail.com>
2022-01-29 23:39:09 +0900

    TRELLO CLONE - refs

--- trello/src/Components/Board.tsx
+++ trello/src/Components/Board.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import { Droppable } from "react-beautiful-dnd";
 import styled from "styled-components";
 import DraggableCard from "./DragabbleCard";
@@ -42,9 +43,19 @@ interface IAreaProps {
 }
 
 function Board({ toDos, boardId }: IBoardProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const onClick = () => {
+    inputRef.current?.focus();
+    setTimeout(() => {
+      inputRef.current?.blur();
+    }, 5000);
+  };
+
   return (
     <Wrapper>
       <Title>{boardId}</Title>
+      <input ref={inputRef} type="text" placeholder="grab me" />
+      <button onClick={onClick}>click me</button>
       <Droppable droppableId={boardId}>
         {(magic, snapshop) => (
           <Area
