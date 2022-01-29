6554fe25f4bb72129d9610a08b6794c4cff536d2
Minna <minna1025@gmail.com>
2022-01-29 23:07:17 +0900

    TRELLO CLONE - cross board movement

--- trello/src/App.tsx
+++ trello/src/App.tsx
@@ -26,6 +26,7 @@ function App() {
   const onDragEnd = (info: DropResult) => {
     console.log(info);
     const { destination, draggableId, source } = info;
+    if (!destination) return;
     if (destination?.droppableId === source.droppableId) {
       // same board movement
       setToDos((allboard) => {
@@ -38,6 +39,21 @@ function App() {
         return {
           ...allboard,
           [source.droppableId]: boardCopy,
+        };
+      });
+    }
+
+    if (destination.droppableId !== source.droppableId) {
+      // cross board movement
+      setToDos((allBoards) => {
+        const sourceBoard = [...allBoards[source.droppableId]];
+        const destinationBoard = [...allBoards[destination.droppableId]];
+        sourceBoard.splice(source.index, 1);
+        destinationBoard.splice(destination?.index, 0, draggableId);
+        return {
+          ...allBoards,
+          [source.droppableId]: sourceBoard,
+          [destination.droppableId]: destinationBoard,
         };
       });
     }
