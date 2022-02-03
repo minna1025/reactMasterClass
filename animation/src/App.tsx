1e94462b2813562804e5514fd4cb2a87d4ce7c79
Minna <minna1025@gmail.com>
2022-02-03 14:26:13 +0900

    ANIMATIONS - gestures part two

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -1,6 +1,7 @@
 import GlobalStyle from "./GlobalStyle";
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
   width: 100vw;
@@ -10,30 +11,47 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`;
+
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: rgba(255, 255, 255, 1);
-  border-radius: 35px;
+  border-radius: 40px;
   box-shadow: 0 20px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 const boxVariants = {
-  hover: { scale: 1.5, rotateZ: 90 },
-  click: { scale: 1, borderRadius: "100px" },
-  drag: { backgroundColor: "rgb(46,204,113)", transition: { dulation: 10 } },
+  hover: { rotateZ: 90 },
+  click: { borderRadius: "100px" },
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
+
   return (
     <Wrapper>
       <GlobalStyle />
-      <Box
-        drag
-        variants={boxVariants}
-        whileHover="hover"
-        whileDrag="drag"
-        whileTap="click"></Box>
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragSnapToOrigin // 드래그 후 원자세로 다시 백
+          dragElastic={0.5}
+          dragConstraints={biggerBoxRef}
+          variants={boxVariants}
+          whileHover="hover"
+          whileDrag="drag"
+          whileTap="click"></Box>
+      </BiggerBox>
     </Wrapper>
   );
 }
