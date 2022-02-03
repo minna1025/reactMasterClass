ecc93c9dfb2b55969a92cb7446a8cccecf1f0740
Minna <minna1025@gmail.com>
2022-02-03 15:30:28 +0900

    ANIMATIONS - motionValues part three

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -1,14 +1,20 @@
 import GlobalStyle from "./GlobalStyle";
 import styled from "styled-components";
-import { motion, useMotionValue, useTransform } from "framer-motion";
+import {
+  motion,
+  useMotionValue,
+  useTransform,
+  useViewportScroll,
+} from "framer-motion";
 import { useEffect } from "react";
 
-const Wrapper = styled.div`
+const Wrapper = styled(motion.div)`
   width: 100vw;
-  height: 100vh;
+  height: 500vh;
   display: flex;
   justify-content: center;
   align-items: center;
+  background: linear-gradient() 135deg, rgb(238, 0, 153), #d0e;
 `;
 
 const Box = styled(motion.div)`
@@ -21,19 +27,22 @@ const Box = styled(motion.div)`
 
 function App() {
   const x = useMotionValue(0);
-  const scale = useTransform(x, [-800, 0, 800], [2, 1, 0.1]); // (받아올 값, 검토할 값, 출력할 값)
-
-  useEffect(() => {
-    scale.onChange(() => {
-      console.log(scale.get());
-    });
-  }, [x]);
-
+  const rotate = useTransform(x, [-800, 800], [-360, 360]);
+  const gradient = useTransform(
+    x,
+    [-800, 800],
+    [
+      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
+      "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
+    ]
+  );
+  const { scrollYProgress } = useViewportScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
   return (
-    <Wrapper>
+    <Wrapper style={{ background: gradient }}>
       <GlobalStyle />
       <Box
-        style={{ x, scale }}
+        style={{ x, rotate, scale }}
         drag="x"
         dragSnapToOrigin // 드래그 후 원자세로 다시 백
       ></Box>
