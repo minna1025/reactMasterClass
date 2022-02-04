9dcf0954a9386d42a4e721a36df5f13336eb4602
Minna <minna1025@gmail.com>
2022-02-05 00:03:08 +0900

    NOMFLIX CLONE - home screen part two

--- minflix/src/api.ts
+++ minflix/src/api.ts
@@ -1,6 +1,25 @@
 const API_KEY = "6168f76b6e8e811fa14c2c1d50c8ec66";
 const BASE_PATH = "https://api.themoviedb.org/3";
 
+interface IMovie {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+
+export interface IGetMoviesResult {
+  dates: {
+    maximum: string;
+    minmum: string;
+  };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
 export function getMovies() {
   return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
     (response) => response.json()
