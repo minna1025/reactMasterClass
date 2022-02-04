0ba1ccdf7f62007c56dbb17890c9e2586c5caa9e
Minna <minna1025@gmail.com>
2022-02-04 13:19:51 +0900

    NOMFLIX CLONE - minflex 초기 셋팅

---
+++ minflex/src/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { RecoilRoot } from "recoil";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
+import { GlobalStyle } from "./GlobalStyle";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { theme } from "./theme";
+
+const client = new QueryClient();
+
+ReactDOM.render(
+  <React.StrictMode>
+    <RecoilRoot>
+      <QueryClientProvider client={client}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <App />
+        </ThemeProvider>
+      </QueryClientProvider>
+    </RecoilRoot>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
