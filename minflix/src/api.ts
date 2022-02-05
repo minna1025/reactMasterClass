79c77c1fe0b992587a88f1b9da3a80be3e279614
Minna <minna1025@gmail.com>
2022-02-05 20:55:43 +0900

    minflix 코드챌린지 작업 시작!

--- minflix/src/api.ts
+++ minflix/src/api.ts
@@ -1,4 +1,4 @@
-const API_KEY = "6168f76b6e8e811fa14c2c1d50c8ec66";
+export const API_KEY = "6168f76b6e8e811fa14c2c1d50c8ec66";
 const BASE_PATH = "https://api.themoviedb.org/3";
 
 interface IMovie {
@@ -21,7 +21,7 @@ export interface IGetMoviesResult {
 }
 
 export function getMovies() {
-  return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
-    (response) => response.json()
-  );
+  return fetch(
+    `${BASE_PATH}/movie/now_playing?api_key=${API_KEY}&language=ko`
+  ).then((response) => response.json());
 }
--- minflix/src/libs.ts
+++ minflix/src/libs.ts
@@ -1,3 +1,7 @@
+import { API_KEY } from "./api";
+
 export function makeImagePath(id: string, format?: string) {
-  return `https://image.tmdb.org/t/p/${format ? format : "original"}/${id}`;
+  return `https://image.tmdb.org/t/p/${
+    format ? format : "original"
+  }/${id}?api_key=${API_KEY}&language=ko&append_to_response=images&include_image_language=ko,null`;
 }
